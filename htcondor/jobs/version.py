eb30974f7aa817008aad987deb193caad8b2773a
Josh Karpel <josh.karpel@gmail.com>
2020-06-01 11:09:56 -0500

    fix coverage include

--- htcondor/jobs/version.py
+++ htcondor/jobs/version.py
@@ -15,13 +15,15 @@
 
 from typing import Tuple, Optional
 
+from . import utils
+
 try:
     from importlib import metadata
 except ImportError:
     # Running on pre-3.8 Python; use importlib-metadata package
     import importlib_metadata as metadata
 
-__version__ = metadata.version("htcondor_jobs")
+__version__ = metadata.version("htcondor-jobs")
 
 
 def version() -> str:
